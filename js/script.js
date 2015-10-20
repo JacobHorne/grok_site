e88828f320c268a9582e58ab1cc5743320eea6d4
jacobhorne <jacobhorne@gmail.com>
2015-10-19 21:04:32 -0400

    grok

---
+++ js/script.js
@@ -0,0 +1,42 @@
+/* js file */
+//javascript self executing anonymous function
+(function( window, undefined ){
+
+	//jquery ready function
+	$( function(){
+
+		var hasAnimatedIn = false;
+		var speakers = $( ".speakers" );
+		var win = $( window );
+		var offset;
+		var top;
+		var height;
+
+		//downCount plugin init
+		$( '.countdown' ).downCount({
+			date : '05/11/2016 06:00:00',
+			offset : +10
+		} );
+
+
+		//speakers.css( "opacity", 0.01 );
+
+		win.scroll( function() {
+
+			offset = speakers.offset().top;
+			top    = win.scrollTop();
+			height = win.height() / 2;
+
+		    if ( top >= offset - height && hasAnimatedIn === false ){
+
+		    	console.log("hello");
+		    	hasAnimatedIn = true;
+		       // speakers.animate( { "opacity" : 1 }, 1000 );
+		       $(speakers).addClass(".fadein");
+
+		    }
+		});
+
+	});
+
+})( window, undefined );
